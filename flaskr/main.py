88c6e2596d85defce8e38b392add448b89db77dd
vicfal414 <vffaltis@buffalo.edu>
2020-10-17 04:39:12 -0400

    revert change procfile again

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,125 +4,61 @@ from flask import Flask
 from flask import render_template
 
 
-# def create_app(test_config=None):
+def create_app(test_config=None):
     # create and configure the app
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-# a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+    @app.route("/")
+    def home():
+        return render_template("index.html")
 
-@app.route("/dash")
-def dash():
-    return render_template("userdashboard.html")
+    @app.route("/dash")
+    def dash():
+        return render_template("userdashboard.html")
 
-@app.route("/challenge")
-def chall():
-    return render_template("challenge.html")
+    @app.route("/challenge")
+    def chall():
+        return render_template("challenge.html")
 
-@app.route("/friends")
-def friend():
-    return render_template("friends.html")
+    @app.route("/friends")
+    def friend():
+        return render_template("friends.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
+    @app.route("/signup")
+    def signup():
+        return render_template("signup.html")
 
-@app.route("/css")
-def css():
-    return render_template("static/css/style.css")
+    @app.route("/css")
+    def css():
+        return render_template("static/css/style.css")
 
-app.run()
+    return app
 
 # if __name__ == "__main__":
 #         app.run()
-# import os
-#
-# from flask import Flask
-# from flask import render_template
-#
-#
-# def create_app(test_config=None):
-#     # create and configure the app
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_mapping(
-#         SECRET_KEY='dev',
-#         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-#     )
-#
-#     if test_config is None:
-#         # load the instance config, if it exists, when not testing
-#         app.config.from_pyfile('config.py', silent=True)
-#     else:
-#         # load the test config if passed in
-#         app.config.from_mapping(test_config)
-#
-#     # ensure the instance folder exists
-#     try:
-#         os.makedirs(app.instance_path)
-#     except OSError:
-#         pass
-#
-#     # a simple page that says hello
-#     @app.route('/hello')
-#     def hello():
-#         return 'Hello, World!'
-#
-#     @app.route("/")
-#     def home():
-#         return render_template("index.html")
-#
-#     @app.route("/dash")
-#     def dash():
-#         return render_template("userdashboard.html")
-#
-#     @app.route("/challenge")
-#     def chall():
-#         return render_template("challenge.html")
-#
-#     @app.route("/friends")
-#     def friend():
-#         return render_template("friends.html")
-#
-#     @app.route("/login")
-#     def login():
-#         return render_template("login.html")
-#
-#     @app.route("/signup")
-#     def signup():
-#         return render_template("signup.html")
-#
-#     @app.route("/css")
-#     def css():
-#         return render_template("static/css/style.css")
-#
-#     return app
-#
-# # if __name__ == "__main__":
-# #         app.run()

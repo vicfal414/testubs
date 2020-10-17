f50dc7457e56a48c880b26a37ccb96c7bf9f64fb
vicfal414 <36310124+vicfal414@users.noreply.github.com>
2020-10-17 16:26:13 -0400

    Update main.py

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,3 +1,88 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import session
+from flask import flash
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/dash")
+    def dash():
+        return render_template("userdashboard.html")
+
+    @app.route("/challenge")
+    def chall():
+        return render_template("challenge.html")
+
+    @app.route("/friends")
+    def friend():
+        return render_template("friends.html")
+
+  #  @app.route("/login")
+  #  def login():
+  #      return render_template("login.html")
+    
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        error = None
+        if request.method == 'POST':
+            if (request.form['username'] != 'test') or request.form['password'] != 'test': error = 'Invalid Credentials. Please try again.'
+            else:
+                session['logged_in'] = True
+                flash('You are logged in.')
+                return redirect(url_for('home'))
+        return render_template('login.html', error=error)
+     
+    @app.route('/logout')
+    def logout():
+        session.pop('logged_in', None)
+        flash('You are logged out.')
+        return redirect(url_for('home'))
+    
+    @app.route("/signup")
+    def signup():
+        return render_template("signup.html")
+
+    @app.route("/css")
+    def css():
+        return render_template("static/css/style.css")
+
+    return app
+
+
+# database
 # import os
 
 # from flask import Flask
@@ -90,64 +175,66 @@
 
 #     return app
 
-import os
 
-from flask import Flask
-from flask import render_template
+# original
+# import os
+
+# from flask import Flask
+# from flask import render_template
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+#     app.config.from_mapping(
+#         SECRET_KEY='dev',
+#         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+#     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+#     if test_config is None:
+#         # load the instance config, if it exists, when not testing
+#         app.config.from_pyfile('config.py', silent=True)
+#     else:
+#         # load the test config if passed in
+#         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+#     # ensure the instance folder exists
+#     try:
+#         os.makedirs(app.instance_path)
+#     except OSError:
+#         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+#     # a simple page that says hello
+#     @app.route('/hello')
+#     def hello():
+#         return 'Hello, World!'
 
-    @app.route("/")
-    def home():
-        return render_template("index.html")
+#     @app.route("/")
+#     def home():
+#         return render_template("index.html")
 
-    @app.route("/dash")
-    def dash():
-        return render_template("userdashboard.html")
+#     @app.route("/dash")
+#     def dash():
+#         return render_template("userdashboard.html")
 
-    @app.route("/challenge")
-    def chall():
-        return render_template("challenge.html")
+#     @app.route("/challenge")
+#     def chall():
+#         return render_template("challenge.html")
 
-    @app.route("/friends")
-    def friend():
-        return render_template("friends.html")
+#     @app.route("/friends")
+#     def friend():
+#         return render_template("friends.html")
 
-    @app.route("/login")
-    def login():
-        return render_template("login.html")
+#     @app.route("/login")
+#     def login():
+#         return render_template("login.html")
 
-    @app.route("/signup")
-    def signup():
-        return render_template("signup.html")
+#     @app.route("/signup")
+#     def signup():
+#         return render_template("signup.html")
 
-    @app.route("/css")
-    def css():
-        return render_template("static/css/style.css")
+#     @app.route("/css")
+#     def css():
+#         return render_template("static/css/style.css")
 
-    return app
+#     return app

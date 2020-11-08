4f0d6a58cdbac6e5751667d50bc06b4f1cd53fe2
vicfal414 <vffaltis@buffalo.edu>
2020-11-08 13:27:26 -0500

    Match dev from actual app

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask import render_template
@@ -7,14 +8,24 @@ from flask import redirect
 from flask import url_for
 from flask import session
 from flask import flash
+from flask_mysqldb import MySQL
+
+import random
+from users import Users
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        MYSQL_HOST='2.tcp.ngrok.io',
+        MYSQL_PORT=15551,
+        MYSQL_USER='root',
+        MYSQL_PASSWORD='passwrd',
+        MYSQL_DB='login'
     )
+
+    mysql = MySQL(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -45,11 +56,11 @@ def create_app(test_config=None):
     @app.route("/challenge")
     def chall():
         return render_template("challenge_list.html")
-    
+
     @app.route("/challenge1")
     def chall_pg1():
         return render_template("challenge_pages/challenge1.html")
-    
+
     @app.route("/challenge2")
     def chall_pg2():
         return render_template("challenge_pages/challenge2.html")
@@ -65,42 +76,135 @@ def create_app(test_config=None):
     @app.route("/challenge5")
     def chall_pg5():
         return render_template("challenge_pages/challenge5.html")
-    
+
     @app.route("/challenge6")
     def chall_pg6():
         return render_template("challenge_pages/challenge6.html")
 
     @app.route("/friends")
     def friend():
-        return render_template("friends.html")
+        return render_template("friends.html",
+            friendList=Users['friends'],
+            notFriendList=Users['notFriends'])
 
-  #  @app.route("/login")
-  #  def login():
-  #      return render_template("login.html")
-    
-    @app.route('/login', methods=['GET', 'POST'])
+    @app.route("/publicProfileFriend")
+    def publicProfileFriend():
+        return render_template("publicProfileFriend.html")
+
+    @app.route("/publicProfileNotFriend")
+    def publicProfileNotFriend():
+        return render_template("publicProfileNotFriend.html")
+
+
+    @app.route("/login", methods = ['GET', 'POST'])
     def login():
-        error = None
-        if request.method == 'POST':
-            if (request.form['username'] != 'test') or request.form['password'] != 'test': error = 'Invalid Credentials. Please try again.'
-            else:
+        msg = ''
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            cur = mysql.connection.cursor()
+            cur.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password, ))
+            data = cur.fetchone()
+            # print(data)
+            if data:
                 session['logged_in'] = True
-                flash('You are logged in.')
+                session['id'] = data[0]
+                session['username'] = data[1]
+                flash('You are logged in')
                 return redirect(url_for('home'))
-        return render_template('login.html', error=error)
-     
+            else:
+                msg = 'Invalid Credentials. Please try again.'
+        return render_template("login.html", msg = msg)
+
+    # @app.route('/login', methods=['GET', 'POST'])
+    # def login():
+    #     error = None
+    #     if request.method == 'POST':
+    #         if (request.form['username'] != 'test') or request.form['password'] != 'test': error = 'Invalid Credentials. Please try again.'
+    #         else:
+    #             session['logged_in'] = True
+    #             flash('You are logged in.')
+    #             return redirect(url_for('home'))
+    #     return render_template('login.html', error=error)
+
     @app.route('/logout')
     def logout():
         session.pop('logged_in', None)
+        session.pop('id', None)
+        session.pop('username', None)
         flash('You are logged out.')
         return redirect(url_for('home'))
 
-    @app.route("/signup")
+    @app.route("/signup", methods = ['GET', 'POST'])
     def signup():
-        return render_template("signup.html")
+        msg = ''
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+            cur = mysql.connection.cursor()
+            cur.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+            data = cur.fetchone()
+            if data:
+                # Account already exists
+                msg = 'Account already exists.'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                # Invalid email address
+                msg = 'Inavlid email address.'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                # Invalid username
+                msg = 'Username must only contain characters and numbers.'
+            elif not username or not password or not email:
+                # Form was not filled out
+                msg = 'Please enter your information.'
+            else:
+                cur.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+                mysql.connection.commit()
+                msg = 'You have successfully registered!'
+        elif request.method == 'POST':
+            #Form is empty
+            msg = 'Please enter your information.'
+        return render_template("signup.html", msg = msg)
 
     @app.route("/css")
     def css():
         return render_template("static/css/style.css")
 
+    @app.route("/random")
+    def rand_chall():
+        number = random.randint(1, 6)
+        site = "chall_pg" + str(number)
+        return redirect(url_for(site))
+
+    def getUserName(name):
+        for uType in Users:
+            for user in Users[uType]:
+                if user['name'] == name:
+                    return user['username']
+        return None
+
+    def getProfilePic(name):
+        for uType in Users:
+            for user in Users[uType]:
+                if user['name'] == name:
+                    return user['profilePic']
+        return None
+
+    @app.route("/addFriend", methods = ['POST'])
+    def addFriend():
+        if request.method == 'POST':
+            name = request.form['notFriend']
+            newFriend = {'username': getUserName(name), 'name': name, 'profilePic':  getProfilePic(name)}
+            Users['friends'].append(newFriend)
+            Users['notFriends'].remove(newFriend)
+        return redirect(url_for('friend'))
+
+    @app.route("/remFriend", methods = ['POST'])
+    def remFriend():
+        if request.method == 'POST':
+            name = request.form['friend']
+            unFriend = {'username': getUserName(name), 'name': name, 'profilePic': getProfilePic(name)}
+            Users['notFriends'].append(unFriend)
+            Users['friends'].remove(unFriend)
+        return redirect(url_for('friend'))
     return app

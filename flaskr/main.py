2e4eb440947ceb296e707ab86c9b1bba7a51fa36
vicfal414 <36310124+vicfal414@users.noreply.github.com>
2020-11-29 23:09:34 -0500

    Update main.py

--- flaskr/main.py
+++ flaskr/main.py
@@ -8,24 +8,31 @@ from flask import redirect
 from flask import url_for
 from flask import session
 from flask import flash
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
+# from flask_mysql import MySQL
+# import pymysql
+import MySQLdb
+import pymysql.cursors
 
 import random
 from users import Users
+
+# mysql = MySQL()
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        MYSQL_HOST='2.tcp.ngrok.io',
-        MYSQL_PORT=15551,
-        MYSQL_USER='root',
-        MYSQL_PASSWORD='passwrd',
-        MYSQL_DB='login'
+        # MYSQL_DATABASE_HOST='us-cdbr-east-02.cleardb.com',
+        # MYSQL_PORT=15551,
+        # MYSQL_DATABSE_USER='b33b6415873ff5',
+        # MYSQL_DATABASE_PASSWORD='d1a1b9a1',
+        # MYSQL_DATABASE_DB='heroku_1e2700f5b989c0b'
     )
 
-    mysql = MySQL(app)
+    # mysql.init_app(app)
+   
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -51,7 +58,7 @@ def create_app(test_config=None):
 
     @app.route("/dash")
     def dash():
-        return render_template("userdashboard.html")
+        return render_template("userdashboard.php")
 
     @app.route("/challenge")
     def chall():
@@ -81,6 +88,11 @@ def create_app(test_config=None):
     def chall_pg6():
         return render_template("challenge_pages/challenge6.html")
 
+    #Addition for Custom Challenge
+    @app.route("/challengeCustom")
+    def chall_pg7():
+        return render_template("challenge_pages/custom_challenge.html")
+
     @app.route("/friends")
     def friend():  
         return render_template("friends.html",
@@ -102,30 +114,28 @@ def create_app(test_config=None):
         if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
             username = request.form['username']
             password = request.form['password']
-            cur = mysql.connection.cursor()
-            cur.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password, ))
-            data = cur.fetchone()
-            # print(data)
+            # cur = mysql.connection.cursor()
+            connection = pymysql.connect(host='us-cdbr-east-02.cleardb.com',
+                             user='b33b6415873ff5',
+                             password='d1a1b9a1',
+                             db='heroku_1e2700f5b989c0b',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password, ))
+            data = cursor.fetchone()
+            print(data)
             if data:
                 session['logged_in'] = True
-                session['id'] = data[0]
-                session['username'] = data[1]
+                session['id'] = data['id']
+                session['username'] = data['username']
                 flash('You are logged in')
                 return redirect(url_for('home'))
             else:
                 msg = 'Invalid Credentials. Please try again.'
+            connection.close()
         return render_template("login.html", msg = msg)
     
-    # @app.route('/login', methods=['GET', 'POST'])
-    # def login():
-    #     error = None
-    #     if request.method == 'POST':
-    #         if (request.form['username'] != 'test') or request.form['password'] != 'test': error = 'Invalid Credentials. Please try again.'
-    #         else:
-    #             session['logged_in'] = True
-    #             flash('You are logged in.')
-    #             return redirect(url_for('home'))
-    #     return render_template('login.html', error=error)
      
     @app.route('/logout')
     def logout():
@@ -142,9 +152,17 @@ def create_app(test_config=None):
             username = request.form['username']
             password = request.form['password']
             email = request.form['email']
-            cur = mysql.connection.cursor()
-            cur.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-            data = cur.fetchone()
+            fname = request.form['fname']
+            lname = request.form['lname']
+            connection2 = pymysql.connect(host='us-cdbr-east-02.cleardb.com',
+                    user='b33b6415873ff5',
+                    password='d1a1b9a1',
+                    db='heroku_1e2700f5b989c0b',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
+            with connection2.cursor() as cursor2:
+                cursor2.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+            data = cursor2.fetchone()
             if data:
                 # Account already exists
                 msg = 'Account already exists.'
@@ -158,12 +176,16 @@ def create_app(test_config=None):
                 # Form was not filled out
                 msg = 'Please enter your information.'
             else:
-                cur.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-                mysql.connection.commit()
+                with connection2.cursor() as cursor3:
+                    cursor3.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)', (fname, lname, username, password, email,))
+                    cursor3.execute('INSERT INTO dashboard VALUES (%s, NULL, NULL, NULL)', (username))
+                connection2.commit()
                 msg = 'You have successfully registered!'
+            connection2.close()
         elif request.method == 'POST':
             #Form is empty
             msg = 'Please enter your information.'
+        
         return render_template("signup.html", msg = msg)
 
     @app.route("/css")
